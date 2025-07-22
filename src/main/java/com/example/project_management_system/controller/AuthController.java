3e14deaf725e35115615213f183a0c2c44ec084b
Rohit <rrgawanderohit@gmail.com>
2025-07-22 17:08:11 +0530

    user signing api done and tested successfully

--- src/main/java/com/example/project_management_system/controller/AuthController.java
+++ src/main/java/com/example/project_management_system/controller/AuthController.java
@@ -1,11 +1,19 @@
 package com.example.project_management_system.controller;
 
+import com.example.project_management_system.config.JwtProvider;
 import com.example.project_management_system.model.User;
 import com.example.project_management_system.repository.UserRepository;
+import com.example.project_management_system.request.LoginRequest;
+import com.example.project_management_system.response.AuthResponse;
 import com.example.project_management_system.service.CustomUserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,8 +30,7 @@ public class AuthController {
   @Autowired
     private CustomUserDetailsImpl customUserDetails;
   @PostMapping("/signup")
-
-  public ResponseEntity<User>createUserHandler(@RequestBody User user) throws Exception{
+  public ResponseEntity<AuthResponse>createUserHandler(@RequestBody User user) throws Exception{
       User isUserExist = userRepository.findByEmail(user.getEmail());
       if(isUserExist!=null){
           throw new Exception("email already exist with another account");
@@ -34,7 +41,41 @@ public class AuthController {
       createdUser.setFullName(user.getFullName());
 
     User savedUser = userRepository.save(createdUser);
-    return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+      Authentication authentication= new UsernamePasswordAuthenticationToken(user.getEmail(),user.getPassword());
+      SecurityContextHolder.getContext().setAuthentication(authentication);
+      String jwt = JwtProvider.generateToken(authentication);
+
+      AuthResponse res = new AuthResponse();
+      res.setMessage("signup success");
+      res.setJwt(jwt);
+    return new ResponseEntity<>(res, HttpStatus.CREATED);
+  }
+@PostMapping("/signing")
+  public ResponseEntity<AuthResponse> signing(@RequestBody LoginRequest loginRequest){
+      String username = loginRequest.getEmail();
+      String password = loginRequest.getPassword();
+
+      Authentication authentication = authenticate(username,password);
+      SecurityContextHolder.getContext().setAuthentication(authentication);
+
+    String jwt = JwtProvider.generateToken(authentication);
+
+    AuthResponse res = new AuthResponse();
+    res.setMessage("signing success");
+    res.setJwt(jwt);
+    return new ResponseEntity<>(res, HttpStatus.CREATED);
+
   }
 
+    private Authentication authenticate(String username, String password) {
+        UserDetails userDetails = customUserDetails.loadUserByUsername(username);
+        if(userDetails==null){
+            throw new BadCredentialsException("invalid username");
+        }
+        if(!passwordEncoder.matches(password,userDetails.getPassword())){
+            throw new BadCredentialsException("invalid password");
+        }
+        return new UsernamePasswordAuthenticationToken
+                (userDetails,null, userDetails.getAuthorities());
+  }
 }
